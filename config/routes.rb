a4ec118c6fd8a959ee04423bc2c58d39b9a30769
Ryan R Algie <ryanalgie@gmail.com>
2015-09-11 16:25:37 -0700

    Setting up landing page
    Playing with the landing page, commit before adding new username
    migration

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'landings#index'
 
+    get "about" => 'pages#about'
+    get "contact" => 'pages#contact'
+    get 'logout' => 'pages#logout'
+    get 'Pins' => 'pins#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
