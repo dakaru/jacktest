61700a41d9a15b8ab812781b15a811390dba57c5
Prelang Builder <builder@prelang.com>
2015-09-11 18:43:54 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Favorites' to allow for create, update

--- app/admin/favorites.rb
+++ app/admin/favorites.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Favorites do
+  permit_params 
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
